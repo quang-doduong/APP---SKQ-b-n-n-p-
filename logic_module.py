2239e80e63b64689162fc380a4d0ac243c8c9057
quang-doduong <quangdo.31251021297@st.ueh.edu.vn>
2026-05-05 21:00:33 +0700

    Cập nhật code

--- logic_module.py
+++ logic_module.py
@@ -1,4 +1,3 @@
-# logic_module.py
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
@@ -7,56 +6,163 @@ from geopy.geocoders import Nominatim
 import osmnx as ox
 import networkx as nx
 
-# --- HỆ THỐNG LOGIC MỜ 27 RULES ---
+# --- 1. KHAI BÁO THÊM BIẾN QUÃNG ĐƯỜNG ---
 mat_do = ctrl.Antecedent(np.arange(0, 11, 1), 'mat_do')           
 loai_xe = ctrl.Antecedent(np.arange(1, 4, 1), 'loai_xe')          
 thoi_gian_doi = ctrl.Antecedent(np.arange(1, 4, 1), 'thoi_gian_doi') 
+quang_duong = ctrl.Antecedent(np.arange(0, 101, 1), 'quang_duong') # Thêm biến quãng đường (0-100km)
 he_so_x = ctrl.Consequent(np.arange(1.0, 3.1, 0.1), 'he_so_x')
 
 mat_do.automf(3, names=['vang', 'vua', 'dong'])
 loai_xe.automf(3, names=['hai_banh', 'taxi_4', 'taxi_7'])
 thoi_gian_doi.automf(3, names=['lau', 'binh_thuong', 'gap'])
 
+# Định nghĩa tập mờ cho Quãng đường
+quang_duong['ngan'] = fuzz.trimf(quang_duong.universe, [0, 0, 5])
+quang_duong['vua'] = fuzz.trimf(quang_duong.universe, [3, 10, 20])
+quang_duong['dai'] = fuzz.trimf(quang_duong.universe, [15, 100, 100])
+
 he_so_x['thap'] = fuzz.trimf(he_so_x.universe, [1.0, 1.0, 1.5])
 he_so_x['vua'] = fuzz.trimf(he_so_x.universe, [1.3, 1.8, 2.3])
 he_so_x['cao'] = fuzz.trimf(he_so_x.universe, [2.0, 3.0, 3.0])
 
-# --- ĐỊNH NGHĨA 27 RULES ---
-# Nhóm 1: Đường vắng
-r1 = ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'], he_so_x['thap'])
-r2 = ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'], he_so_x['thap'])
-r3 = ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'], he_so_x['vua'])
-r4 = ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'], he_so_x['thap'])
-r5 = ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'], he_so_x['vua'])
-r6 = ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'], he_so_x['cao'])
-r7 = ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'], he_so_x['vua'])
-r8 = ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'], he_so_x['cao'])
-r9 = ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'], he_so_x['cao'])
-# Nhóm 2: Đường vừa
-r10 = ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'], he_so_x['thap'])
-r11 = ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'], he_so_x['vua'])
-r12 = ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'], he_so_x['cao'])
-r13 = ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'], he_so_x['vua'])
-r14 = ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'], he_so_x['vua'])
-r15 = ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'], he_so_x['cao'])
-r16 = ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'], he_so_x['cao'])
-r17 = ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'], he_so_x['cao'])
-r18 = ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'], he_so_x['cao'])
-# Nhóm 3: Đường đông
-r19 = ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'], he_so_x['vua'])
-r20 = ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'], he_so_x['cao'])
-r21 = ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'], he_so_x['cao'])
-r22 = ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'], he_so_x['cao'])
-r23 = ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'], he_so_x['cao'])
-r24 = ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'], he_so_x['cao'])
-r25 = ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'], he_so_x['cao'])
-r26 = ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'], he_so_x['cao'])
-r27 = ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'], he_so_x['cao'])
 
-tinh_gia_logic = ctrl.ControlSystem([r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12,r13,r14,r15,r16,r17,r18,r19,r20,r21,r22,r23,r24,r25,r26,r27])
+
+#Toàn bộ 81 luật được tạo ở đây
+# 27 luật cho đường vắng
+rules_vang = [
+    # Xe 2 bánh (9 luật)
+    ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'] & quang_duong['ngan'], he_so_x['thap']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'] & quang_duong['vua'], he_so_x['thap']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'] & quang_duong['dai'], he_so_x['thap']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'] & quang_duong['ngan'], he_so_x['thap']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'] & quang_duong['vua'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'] & quang_duong['dai'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'] & quang_duong['ngan'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'] & quang_duong['vua'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'] & quang_duong['dai'], he_so_x['cao']),
+
+    # Taxi 4 chỗ (9 luật)
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'] & quang_duong['ngan'], he_so_x['thap']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'] & quang_duong['vua'], he_so_x['thap']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'] & quang_duong['dai'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'] & quang_duong['ngan'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'] & quang_duong['vua'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'] & quang_duong['dai'], he_so_x['cao']),
+
+    # Taxi 7 chỗ (9 luật)
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'] & quang_duong['ngan'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'] & quang_duong['vua'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vang'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'] & quang_duong['dai'], he_so_x['cao']),
+]
+
+# 27 luật cho đường mật độ trung bình
+rules_vua = [
+    # Xe 2 bánh (9 luật)
+    ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'] & quang_duong['ngan'], he_so_x['thap']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'] & quang_duong['vua'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'] & quang_duong['dai'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'] & quang_duong['ngan'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'] & quang_duong['vua'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'] & quang_duong['dai'], he_so_x['cao']),
+
+    # Taxi 4 chỗ (9 luật)
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'] & quang_duong['ngan'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'] & quang_duong['vua'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'] & quang_duong['ngan'], he_so_x['vua']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'] & quang_duong['dai'], he_so_x['cao']),
+
+    # Taxi 7 chỗ (9 luật)
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['vua'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'] & quang_duong['dai'], he_so_x['cao']),
+]
+# 27 luật cho đường đông đúc 
+rules_dong = [
+    # --- XE HAI BÁNH (9 luật) ---
+    ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'] & quang_duong['ngan'], he_so_x['thap']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'] & quang_duong['vua'], he_so_x['vua']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['lau'] & quang_duong['dai'], he_so_x['vua']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'] & quang_duong['ngan'], he_so_x['vua']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['binh_thuong'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['hai_banh'] & thoi_gian_doi['gap'] & quang_duong['dai'], he_so_x['cao']),
+
+    # --- TAXI 4 CHỖ (9 luật) ---
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'] & quang_duong['ngan'], he_so_x['vua']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'] & quang_duong['vua'], he_so_x['vua']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['lau'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'] & quang_duong['ngan'], he_so_x['vua']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['binh_thuong'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_4'] & thoi_gian_doi['gap'] & quang_duong['dai'], he_so_x['cao']),
+
+    # --- TAXI 7 CHỖ (9 luật) ---
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'] & quang_duong['ngan'], he_so_x['vua']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['lau'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['binh_thuong'] & quang_duong['dai'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'] & quang_duong['ngan'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'] & quang_duong['vua'], he_so_x['cao']),
+    ctrl.Rule(mat_do['dong'] & loai_xe['taxi_7'] & thoi_gian_doi['gap'] & quang_duong['dai'], he_so_x['cao']),
+]
+
+
+# gộp tất cả lại 
+all_rules = rules_vang + rules_vua + rules_dong
+tinh_gia_logic = ctrl.ControlSystem(all_rules)
 app_tinh_phi = ctrl.ControlSystemSimulation(tinh_gia_logic)
 
+
+
 def calculate_trip(A, B, xe_val, doi_val):
+    # 1. Geocode và tính Quãng đường TRƯỚC
+    # geolocator = Nominatim(user_agent="taxi_app_final")
+    geolocator = Nominatim(user_agent="my_unique_taxi_app_2024", timeout=10)
+    loc_A = geolocator.geocode(A)
+    loc_B = geolocator.geocode(B)
+    if not loc_A or not loc_B:
+        raise ValueError("Không tìm thấy địa chỉ. Hãy thử ghi rõ Tên Đường, Quận, Thành phố.")
+
+    midpoint = ((loc_A.latitude + loc_B.latitude) / 2, (loc_A.longitude + loc_B.longitude) / 2)
+    G = ox.graph_from_point(midpoint, dist=5000, network_type='drive') 
+    orig = ox.distance.nearest_nodes(G, loc_A.longitude, loc_A.latitude)
+    dest = ox.distance.nearest_nodes(G, loc_B.longitude, loc_B.latitude)
+    
+    route = nx.shortest_path(G, orig, dest, weight="length")
+    coords = [(G.nodes[n]['y'], G.nodes[n]['x']) for n in route]
+    km = nx.path_weight(G, route, weight='length') / 1000
+
+    # 2. Đưa dữ liệu vào hệ thống mờ (bao gồm cả km vừa tính)
     now = datetime.datetime.now()
     current_hour = now.hour
     if 7 <= current_hour <= 9 or 17 <= current_hour <= 19:
@@ -69,23 +175,12 @@ def calculate_trip(A, B, xe_val, doi_val):
     app_tinh_phi.input['mat_do'] = mat_do_thuc_te
     app_tinh_phi.input['loai_xe'] = xe_val
     app_tinh_phi.input['thoi_gian_doi'] = doi_val
+    app_tinh_phi.input['quang_duong'] = km # Truyền tham số thứ 4
+    
     app_tinh_phi.compute()
     X = app_tinh_phi.output['he_so_x']
 
-    geolocator = Nominatim(user_agent="taxi_app_final")
-    loc_A = geolocator.geocode(A)
-    loc_B = geolocator.geocode(B)
-    if not loc_A or not loc_B:
-        raise ValueError("Không tìm thấy địa chỉ. Hãy thử ghi rõ Tên Đường, Quận, Thành phố.")
-
-    midpoint = ((loc_A.latitude + loc_B.latitude) / 2, (loc_A.longitude + loc_B.longitude) / 2)
-    G = ox.graph_from_point(midpoint, dist=5000, network_type='drive')
-    orig = ox.distance.nearest_nodes(G, loc_A.longitude, loc_A.latitude)
-    dest = ox.distance.nearest_nodes(G, loc_B.longitude, loc_B.latitude)
-    
-    route = nx.shortest_path(G, orig, dest, weight="length")
-    coords = [(G.nodes[n]['y'], G.nodes[n]['x']) for n in route]
-    km = nx.path_weight(G, route, weight='length') / 1000
+    # 3. Tính tiền
     price = km * 10000 * X
 
     return tinh_trang, km, X, price, (loc_A.latitude, loc_A.longitude), (loc_B.latitude, loc_B.longitude), coords, midpoint
